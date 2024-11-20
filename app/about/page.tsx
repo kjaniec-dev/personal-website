6badbba466e3280c132323d171b5cf064ff1366d
Krzysztof Janiec <contact@kjaniec.dev>
2024-11-20 17:55:10 +0100

    Restored my website from previous repository to newest template

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,6 +3,13 @@ import { MDXLayoutRenderer } from 'pliny/mdx-components'
 import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
+import SectionLayout from '@/layouts/SectionLayout'
+import TimelineEntry from '@/components/TimelineEntry'
+import Skill from '@/components/Progress'
+import experienceData from '@/data/experienceData'
+import skillsData from '@/data/skillsData'
+import educationData from '@/data/educationData'
+// import dynamic from 'next/dynamic'
 
 export const metadata = genPageMetadata({ title: 'About' })
 
@@ -10,11 +17,47 @@ export default function Page() {
   const author = allAuthors.find((p) => p.slug === 'default') as Authors
   const mainContent = coreContent(author)
 
+  // const Map = dynamic(() => import('../../components/Map'), {})
+
   return (
     <>
       <AuthorLayout content={mainContent}>
         <MDXLayoutRenderer code={author.body.code} />
       </AuthorLayout>
+
+      {/*<Map />*/}
+
+      <SectionLayout title={'Experience'}>
+        <div className="container mt-8">
+          <div className="flex grid-cols-12 flex-col text-gray-50 md:grid">
+            {experienceData.map((experienceDataEntry, index) => (
+              <TimelineEntry key={`experience-entry-${index}`} entry={experienceDataEntry} />
+            ))}
+          </div>
+        </div>
+      </SectionLayout>
+
+      <SectionLayout title="Skills">
+        <div className="my-4 flex flex-wrap items-center justify-around gap-2">
+          {skillsData.map((skill, index) => (
+            <Skill key={`skill-${index}`} name={skill.title} percent={skill.percent} />
+          ))}
+        </div>
+      </SectionLayout>
+
+      <SectionLayout title={'Education'}>
+        <div className="container mt-8">
+          <div className="flex grid-cols-12 flex-col text-gray-50 md:grid">
+            {educationData.map((educationEntry, index) => (
+              <TimelineEntry key={`education-entry-${index}`} entry={educationEntry}>
+                <p className="my-2 text-justify leading-tight text-gray-500">
+                  {'Specialization: Software Engineering'}
+                </p>
+              </TimelineEntry>
+            ))}
+          </div>
+        </div>
+      </SectionLayout>
     </>
   )
 }
---
+++ data/experienceData.ts
@@ -0,0 +1,59 @@
+interface ExperienceSectionEntry {
+  primaryText: string
+  secondaryText: string
+  tags?: string[]
+  link?: { href: string; text: string }
+}
+
+const experienceData: ExperienceSectionEntry[] = [
+  {
+    primaryText: 'AMB Software IT Services Sp. z o.o',
+    secondaryText: '2024 - Present',
+    tags: [
+      'Java',
+      'Typescript',
+      'Spring Boot',
+      'Angular',
+      'DB2',
+      'Kafka',
+      'Keycloak',
+      'Docker',
+      'Kubernetes',
+    ],
+  },
+  {
+    primaryText: 'Biot Sp. z o.o',
+    secondaryText: '2019 - 2024',
+    tags: [
+      'Typescript',
+      'Node.JS',
+      'Express.JS',
+      'Nest.JS',
+      'React',
+      'GRPC',
+      'PostgreSQL',
+      'RabbitMQ',
+      'Redis',
+      'ElasticSearch',
+      'Material.UI',
+      'Mapbox.GL',
+      'Deck.GL',
+      'Docker',
+      'Kubernetes',
+    ],
+    link: { href: 'https://biotcloud.com', text: 'biotcloud' },
+  },
+  {
+    primaryText: 'Cinkciarz.pl',
+    secondaryText: '2015 - 2019',
+    tags: ['Java', 'Spring Boot', 'Typescript', 'Angular', 'PostgreSQL', 'Bootstrap', 'Docker'],
+    link: { href: 'https://forex.cinkciarz.pl', text: 'Forex' },
+  },
+  {
+    primaryText: 'Trax S.A. / Icomp Sp. z o.o.',
+    secondaryText: '2013 - 2015',
+    tags: ['Infor Syteline', 'Visual Basic', 'T-SQL'],
+  },
+]
+
+export default experienceData
