e7e1c3b4398b66b3370efb3004956b86cfc52685
Krzysztof Janiec <contact@kjaniec.dev>
2024-11-20 17:55:10 +0100

    Restored my website from previous repository to newest template

---
+++ data/skillsData.ts
@@ -0,0 +1,17 @@
+interface Skill {
+  title: string
+  percent: number
+}
+
+const skillsData: Skill[] = [
+  { title: 'Java', percent: 90 },
+  { title: 'Typescript', percent: 90 },
+  { title: 'Spring Boot', percent: 85 },
+  { title: 'Node.JS', percent: 90 },
+  { title: 'PostgreSQL', percent: 90 },
+  { title: 'React', percent: 95 },
+  { title: 'Angular', percent: 85 },
+  { title: 'Docker', percent: 90 },
+]
+
+export default skillsData
